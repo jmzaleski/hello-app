eadca334989c57669c50d562e46506487333312d
Mathew Zaleski <matz@zaleski.ca>
2018-06-01 17:11:11 -0400

    follow recipe to make into hello world rails app
    http://guides.rubyonrails.org/getting_started.html

--- test/controllers/hello_controller_test.rb
+++ test/controllers/hello_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class HelloControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get hello_index_url
+    assert_response :success
+  end
+
 end
